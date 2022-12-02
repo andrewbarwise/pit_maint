772113efce2330690f3f9ead6803ece92a730e71
Andrew Barwise <andrew.barwise@gmail.com>
2022-12-02 15:23:05 +0800

    minor changes

--- src/data_cleanse.py
+++ src/data_cleanse.py
@@ -5,6 +5,6 @@ path_to_file = "C:\data\pit_maint\data\pit_location.xlsx"
 
 def data(path_to_file):
     # read file
-    req_cols = 'A,B,C,F'
+    req_cols = 'A,B,C,D'
     df = pd.read_excel(path_to_file, usecols = req_cols, engine = "openpyxl")
     return df
