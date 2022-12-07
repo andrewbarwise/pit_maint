c62af817f27033f0b0d77b44a126706f77e00e23
Andrew <andrew.barwise@westernpower.com.au>
2022-12-07 09:44:49 +0800

    changes to file upload and functionality to upload images directly from camera

--- pages/form.py
+++ pages/form.py
@@ -9,6 +9,9 @@ st.session_state.df = data("C:\data\pit_maint\data\pit_location.xlsx")
 st.session_state.service_update_df = pd.read_csv("C:\data\pit_maint\data\service_update.csv")
 
 pit_id = st.session_state.df['Fomat PIT'].values.tolist()
+# get current date
+today = date.today()
+date_updated = today.strftime("%d/%m/%Y")
 
 with st.form("Form inputs", clear_on_submit=True):
     pits = st.selectbox("Pit ID",pit_id)
@@ -28,29 +31,13 @@ with st.form("Form inputs", clear_on_submit=True):
 
     comments = st.text_input("Please add any additional comments.")
 
-    photo = st.file_uploader('Upload an image', type=["jpg", "jpeg","jfif"])
-
-    if photo is not None:
-        image = Image.open(photo)
-        st.image(image, caption='Uploaded image.', use_column_width=True)
-        st.write("")
-
     submitted = st.form_submit_button("Submit")
 
-    if submitted:
-        # get current date
-        today = date.today()
-        date_updated = today.strftime("%d/%m/%Y")
-    
+    if submitted:    
         new_entry = [{'Pit Id' : pits, 'Date' : today, 'Address' : location, 'Accessible' : access, 'Debris Free' : debris, \
             'Lid Level' : level, 'Lid Intact' : pit_lid, 'Collar Intact' : collar, 'Lid Locked' : lock, 'Pit Labelled' : label, \
                 'PitLok' : pitlok, 'Comments' : comments}]
         st.session_state.service_update_df = st.session_state.service_update_df.append(new_entry)
-
-        if not os.path.exists(f'data\photos\{pits}'):
-            os.makedirs(f'data\photos\{pits}')
-
-        image.save(f'data\photos\{pits}\{today}.jpg')
 
         try:
             st.session_state.service_update_df.to_csv("C:\data\pit_maint\data\service_update.csv", index = False)
@@ -59,4 +46,29 @@ with st.form("Form inputs", clear_on_submit=True):
         except PermissionError:
             st.caption("Please close the file and then reclick the Update button. ")
 
+# CREATE FUNCTIONALITY TO UPLOAD PHOTOS
+# file upload 
+file_image = st.file_uploader('Upload an image', type=["jpg", "jpeg","jfif"], accept_multiple_files= True)
+    
+if file_image is not None:
+    image = Image.open(file_image)
+    st.image(image, caption='Uploaded image.', use_column_width=True)
+    st.write("")
 
+    if not os.path.exists(f'data\photos\{pits}'):
+        os.makedirs(f'data\photos\{pits}')
+
+    image.save(f'data\photos\{pits}\{today}.jpg')
+
+# UPLOAD FROM CAMERA
+camera_image = st.camera_input("Take a picture")
+
+if camera_image is not None:
+    img = Image.open(camera_image)
+    st.image(image, caption='Uploaded image.', use_column_width=True)
+    st.write("")
+
+    if not os.path.exists(f'data\photos\{pits}'):
+        os.makedirs(f'data\photos\{pits}')
+
+    image.save(f'data\photos\{pits}\{today}.jpg')
